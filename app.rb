e1a078fded69b35ea3f76cef590874077b8b9db1
Graham Ashton <graham@effectif.com>
2010-11-28 22:49:13 +0000

    Rely on default actions where possible.

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 module Nesta
   class App
-    # Use assets in theme > public directory instead of root public directory.
     use Rack::Static, :urls => ["/painter"], :root => "themes/painter/public"
+
+    helpers do
+      def load_snippet(file)
+        File.new(file).read if File.exist? file
+      end
+    end
+
+    get "/stylesheets/:sheet.css" do
+      content_type "text/css", :charset => "utf-8"
+      cache scss(params[:sheet].to_sym)
+    end
   end
 end
